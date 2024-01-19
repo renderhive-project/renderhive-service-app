f78119a7e64b447e9aafc0f18c64edadfceead0b
Christian Stolze <reg.cs@t-online.de>
2024-01-19 23:22:52 +0100

    Added: isNode() to the ContractService
    The JSON-RPC can now be used to execute the following smart contract functions via the frontend JSON-RPC playground for testing purposes:
    
    - isNode

--- backend/src/jsonrpc/contracts.go
+++ backend/src/jsonrpc/contracts.go
@@ -537,7 +537,7 @@ func (ops *ContractService) GetReservedOperatorFunds(r *http.Request, args *GetR
 	if err != nil {
 		return fmt.Errorf("Error getting contract response record: %v", err)
 	}
-	reply.Message = "getOperatorBalance function was called with transaction: " + response.TransactionID.String() + "\n\n" + fmt.Sprintf("Result: %v", amount)
+	reply.Message = "getReservedOperatorFunds function was called with transaction: " + response.TransactionID.String() + "\n\n" + fmt.Sprintf("Result: %v", amount)
 
 	// create reply for the RPC client
 	return nil
@@ -786,6 +786,100 @@ func (ops *ContractService) RemoveNode(r *http.Request, args *RemoveNodeArgs, re
 
 	// set a reply message
 	reply.Message = "removeNode function was called with transaction: " + response.TransactionID.String()
+
+	// create reply for the RPC client
+	return nil
+
+}
+
+// Method: isNode
+// 			- check if the given node is registered in the Renderhive Smart Contract
+// #############################################################################
+
+// Arguments and reply
+type IsNodeArgs struct {
+	ContractID        string // the ID of the smart contract
+	NodeAccountID     string // the account ID of the node to query for
+	OperatorAccountID string // the account ID of the operator owning the node
+
+	Gas uint64 // the gas limit for the transaction
+}
+type IsNodeReply struct {
+	Message string
+	Value   bool
+}
+
+// Method
+func (ops *ContractService) IsNode(r *http.Request, args *IsNodeArgs, reply *IsNodeReply) error {
+	var err error
+
+	// lock the mutex
+	Manager.Mutex.Lock()
+	defer Manager.Mutex.Unlock()
+
+	// TODO: Implement further checks and security measures
+
+	// log info
+	logger.Manager.Package["jsonrpc"].Info().Msg(fmt.Sprintf("Calling a smart contract function (Gas: %v)", args.Gas))
+
+	// prepare the contract object
+	contractID, err := hederasdk.ContractIDFromString(args.ContractID)
+	if err != nil {
+		return fmt.Errorf("Error: %v", err)
+	}
+	contract := hedera.HederaSmartContract{ID: contractID}
+
+	// prepare the passed AccountIDs
+	operatorAccountID, err := hederasdk.AccountIDFromString(args.OperatorAccountID)
+	if err != nil {
+		return fmt.Errorf("Error: %v", err)
+	}
+	nodeAccountID, err := hederasdk.AccountIDFromString(args.NodeAccountID)
+	if err != nil {
+		return fmt.Errorf("Error: %v", err)
+	}
+
+	// prepare the parameters for the function call
+	params := hederasdk.NewContractFunctionParameters()
+
+	// add operator account ID to the parameters
+	params, err = params.AddAddress(operatorAccountID.ToSolidityAddress())
+	if err != nil {
+		return fmt.Errorf("Error: %v", err)
+	}
+
+	// add node account ID to the parameters
+	params, err = params.AddAddress(nodeAccountID.ToSolidityAddress())
+	if err != nil {
+		return fmt.Errorf("Error: %v", err)
+	}
+
+	// call the function
+	response, receipt, err := contract.CallFunction("isNode", params, args.Gas)
+	fmt.Println("Response:", response)
+	fmt.Println("Receipt:", receipt)
+	fmt.Println("Error:", err)
+	if err != nil {
+		return fmt.Errorf("Error: %v", err)
+	}
+
+	// log info
+	logger.Manager.Package["jsonrpc"].Info().Msg(fmt.Sprintf(" [#] Contract function called with transaction: %v", response.TransactionID.String()))
+
+	// get the result of the function call
+	record, err := response.GetRecord(hedera.Manager.NetworkClient)
+	if err != nil {
+		return fmt.Errorf("Error getting contract response record: %v", err)
+	}
+
+	functionResult, err := record.GetContractExecuteResult()
+	if err != nil {
+		return fmt.Errorf("Error getting contract execute result: %v", err)
+	}
+
+	// set a reply message
+	reply.Value = functionResult.GetBool(0)
+	reply.Message = "IsNode function was called with transaction: " + response.TransactionID.String() + "\n\n" + fmt.Sprintf("Result: %v", reply.Value)
 
 	// create reply for the RPC client
 	return nil
